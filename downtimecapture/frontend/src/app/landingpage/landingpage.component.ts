92f5080adc68a87b6c7e5b475ab0fdfbb93cf92e
studsopjlu3404 <luka.sopjanac@stud.th-rosenheim.de>
2023-05-14 16:16:34 +0200

    US 15 - Foto aus Galerie auswählen und hochladen

--- downtimecapture/frontend/src/app/landingpage/landingpage.component.ts
+++ downtimecapture/frontend/src/app/landingpage/landingpage.component.ts
@@ -19,9 +19,7 @@ export class LandingpageComponent implements OnInit{
   name: string;
   surname: string;
   imgToSave: File;
-  public imagePath: any;
   imgURL: any;
-  public message: string;
   mediatimestamp: string;
   comment: string = '';
 
@@ -42,41 +40,61 @@ export class LandingpageComponent implements OnInit{
     if (files.length === 0)
       return;
  
-    //Prüfen ob Datei in Varibale abgespeichert wurde und es ein Bild ist 
+    //Prüfen ob Datei in Variable abgespeichert wurde und es ein Bild ist 
     var mimeType = files[0].type;
     if (mimeType.match(/image\/*/) == null) {
-      this.message = "Only images are supported.";    
+      alert("Only images are supported.");
       break preview;  
     }    
 
     //HTML-Input-Elemente aufrufen
-      const fileInput = document.getElementById("captureimg") as HTMLInputElement;
-      const file = fileInput.files!.item(0)!;
+    const captureImgInput = document.getElementById("captureimg") as HTMLInputElement;
+    const galleryImgInput = document.getElementById("galleryimg") as HTMLInputElement;
     
-      //Nur gängige Formate erlaubt
-      if (file.type !== "image/jpeg" && file.type !== "image/png" && file.type !== "image/jpg") {
-        alert("Only common photo formats (jpeg, png and jpg) are allowed.");
-        break preview;
-      }
+    let file: File | null = null;
 
-      //Variable, die den TimeStamp zwischenspeichert
-      this.mediatimestamp = new Date().toString();
-      //alert(this.mediatimestamp); //Auskommentieren zum testen und bei deleteImage auch
-      
-      //Foto kleiner als 5MB
-      if (file.size > 5 * 1024 * 1024) {
-        alert("The photo must not be larger than 5 MB.");
-        break preview;
-      }
-      
-      
-      this.imgToSave = file;
-      console.log(this.imgToSave.name)
-
-      const formData = new FormData();
-      formData.append("photo", file);
+    if (captureImgInput.files && captureImgInput.files[0]) {
+    file = captureImgInput.files[0];
+    } else if (galleryImgInput.files && galleryImgInput.files[0]) {
+    file = galleryImgInput.files[0];
+    }
     
-      // fetch("/upload", {
+    if (!file) {
+      break preview;
+    }
+    
+    //Nur gängige Formate erlaubt
+    if (file.type !== "image/jpeg" && file.type !== "image/png" && file.type !== "image/jpg") {
+      alert("Only common photo formats (jpeg, png and jpg) are allowed.");
+      break preview;
+    }
+      
+    //Foto kleiner als 5MB
+    if (file.size > 5 * 1024 * 1024) {
+      alert("The photo must not be larger than 5 MB.");
+      break preview;
+    }
+
+    //Foto nicht älter als 12 Stunden
+    const ageInMs = Date.now() - file.lastModified;
+    const ageInHours = ageInMs / (1000 * 60 * 60);
+    if (ageInHours > 12) {
+    alert("The photo must not be older than 12 hours.");
+    return;
+    }
+
+    //TimeStamp zwischenspeichern
+    this.mediatimestamp = new Date(file.lastModified).toString();
+    //alert(this.mediatimestamp); //Auskommentieren zum testen und bei deleteImage auch
+      
+
+    this.imgToSave = file;
+    console.log(this.imgToSave.name)
+
+    const formData = new FormData();
+    formData.append("photo", file);
+    
+    // fetch("/upload", {
       //   method: "POST",
       //   body: formData
       // })
@@ -90,14 +108,11 @@ export class LandingpageComponent implements OnInit{
       // .catch(error => {
       //   alert("Beim Hochladen des Fotos ist ein Fehler aufgetreten.");
       // });
-    
- 
     var reader = new FileReader();
-    this.imagePath = files;
     reader.readAsDataURL(files[0]); 
     reader.onload = (_event) => { 
-      this.imgURL = reader.result; 
-      const imageData = reader.result!.toString();
+    this.imgURL = reader.result;
+    const imageData = reader.result!.toString();
 
     }
   }
@@ -107,29 +122,25 @@ export class LandingpageComponent implements OnInit{
   deleteImage() {
     
     this.imgURL = null;
-    this.imagePath = null;
 
-    const fileInput = document.getElementById("captureimg") as HTMLInputElement;
-    if (fileInput) {
-        fileInput.value = ""; //Foto aus Frontend löschen
-    }
+    //Foto löschen
+    const inputs = ["captureimg", "galleryimg"];
+    inputs.forEach((input) => {
+      const element = document.getElementById(input) as HTMLInputElement;
+      if (element) {
+        element.value = "";
+      }
+    });
 
-    this.mediatimestamp= ''; //Foto Zeitstempel löschen
-
-    const commentInput = document.getElementById("commentdtc") as HTMLInputElement;
-    if (commentInput) {
-        commentInput.value = ""; //Kommentarfeld löschen
-    }
-
-    this.charCount = 0; //Kommentar Anzahl Zähler auf '0'
+    this.comment = '';        //Kommentar löschen
+    this.charCount = 0;       //Kommentarzeichen Anzahl Zähler auf '0'
+    this.mediatimestamp= '';  //Foto Zeitstempel löschen
     
     console.log('Media deleted successfully.')
     //alert(this.mediatimestamp); //Bei MediaTimeStamp auch auskommentieren zum testen
   }
 
   //Kommentar Maximale Zeichenanzahl und Popup
-
-  
   charCount: number = 0;
   
   updateCharCount(event: any) {
@@ -205,4 +216,3 @@ export class LandingpageComponent implements OnInit{
   }
 
 }
-
