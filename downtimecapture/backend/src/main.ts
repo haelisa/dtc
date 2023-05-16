38c160e3ad8e24838016b739be1de8da73db6659
studpritth7069 <thomas.pritzkau@stud.th-rosenheim.de>
2023-05-16 11:12:39 +0200

    US 7 - Bugfixes MediaTimeStamp: timezone

--- downtimecapture/backend/src/main.ts
+++ downtimecapture/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
+import * as moment from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,9 @@ async function bootstrap() {
   // app.enableCors({
   //   origin: ['from', 'to']
   // })
+
+  // Setze die Zeitzone
+  moment.tz.setDefault('Europe/Berlin');
   
   await app.listen(process.env.DB_PORT || 3000);
 }
